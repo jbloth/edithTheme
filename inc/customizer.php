827f3125643ed2d340860375e52d4827a5259995
Juliane <julianewonka@Julianes-MBP.fritz.box>
2020-07-23 08:57:03 +0200

    Added color settings and live preview to customizer

--- inc/customizer.php
+++ inc/customizer.php
@@ -1,5 +1,6 @@
 <?php  
 
+/* ---- register customizer controls and settings ---- */
 function edith_customizer ( $wp_customize){
 
     // Copyright 
@@ -21,6 +22,130 @@ function edith_customizer ( $wp_customize){
         'settings' => 'set_copyright',
         'type' => 'text'
     ));
+
+    /* ======== Colors ======== */
+    $wp_customize->add_section( 'cd_colors' , array(
+        'title'      => 'Colors',
+        'priority'   => 30,
+    ));
+
+    // background color
+    $wp_customize->add_setting( 'main_background_color' , array(
+        'default'     => '#ffffff',
+        'sanitize_callback' => 'sanitize_hex_color', // Use sanitizer bc sometimes the hash is omitted from the color
+        'transport'   => 'postMessage', // call function from customizer.js setting is changed
+    ));
+
+    $wp_customize->add_control( 
+        new WP_Customize_Color_Control( $wp_customize, 'main_background_color', array(
+            'label'      => 'Background Color',
+            'section'    => 'cd_colors',
+            'settings'   => 'main_background_color',
+        ) 
+    ));
+
+    // header background color
+    $wp_customize->add_setting( 'header_background_color' , array(
+        'default'     => '#f5f5ed',
+        'sanitize_callback' => 'sanitize_hex_color',
+        'transport'   => 'postMessage', 
+    ));
+
+    $wp_customize->add_control( 
+        new WP_Customize_Color_Control( $wp_customize, 'header_background_color', array(
+            'label'      => 'Header Background Color',
+            'section'    => 'cd_colors',
+            'settings'   => 'header_background_color',
+        ) 
+    ));
+
+    // footer background color
+    $wp_customize->add_setting( 'footer_background_color' , array(
+        'default'     => '#f5f5ed',
+        'sanitize_callback' => 'sanitize_hex_color',
+        'transport'   => 'postMessage', 
+    ));
+
+    $wp_customize->add_control( 
+        new WP_Customize_Color_Control( $wp_customize, 'footer_background_color', array(
+            'label'      => 'Footer Background Color',
+            'section'    => 'cd_colors',
+            'settings'   => 'footer_background_color',
+        ) 
+    ));
+
+     // main text color
+    $wp_customize->add_setting( 'main_text_color' , array(
+        'default'     => '#2f2f2f',
+        'sanitize_callback' => 'sanitize_hex_color', // Use sanitizer bc sometimes the hash is omitted from the color
+        'transport'   => 'postMessage', // call function from customizer.js setting is changed
+    ));
+
+    $wp_customize->add_control( 
+        new WP_Customize_Color_Control( $wp_customize, 'main_text_color', array(
+            'label'      => 'Text Color',
+            'section'    => 'cd_colors',
+            'settings'   => 'main_text_color',
+        ) 
+    ));
+
+    // header text color
+    $wp_customize->add_setting( 'header_text_color' , array(
+        'default'     => '#2f2f2f',
+        'sanitize_callback' => 'sanitize_hex_color',
+        'transport'   => 'postMessage', 
+    ));
+
+    $wp_customize->add_control( 
+        new WP_Customize_Color_Control( $wp_customize, 'header_text_color', array(
+            'label'      => 'Header Text Color',
+            'section'    => 'cd_colors',
+            'settings'   => 'header_text_color',
+        ) 
+    ));
+
+    // footer text color
+    $wp_customize->add_setting( 'footer_text_color' , array(
+        'default'     => '#2f2f2f',
+        'sanitize_callback' => 'sanitize_hex_color',
+        'transport'   => 'postMessage', 
+    ));
+
+    $wp_customize->add_control( 
+        new WP_Customize_Color_Control( $wp_customize, 'footer_text_color', array(
+            'label'      => 'Footer Text Color',
+            'section'    => 'cd_colors',
+            'settings'   => 'footer_text_color',
+        ) 
+    ));
 }
 
 add_action( 'customize_register', 'edith_customizer');
+
+
+/* ---- change css according to settings ---- */
+function edith_customizer_css()
+{
+    ?>
+         <style type="text/css">
+            /* body/main colors */
+            body { 
+                background-color: <?php echo get_theme_mod('main_background_color', '#ffffff'); ?>;
+                color: <?php echo get_theme_mod('main_text_color', '#2f2f2f'); ?>;  
+            }
+
+            /* header colors */
+            .site-header { 
+                background-color: <?php echo get_theme_mod('header_background_color', '#f5f5ed'); ?>; 
+                color: <?php echo get_theme_mod('header_text_color', '#2f2f2f'); ?>;  
+            }
+
+            /* footer colors */
+            footer { 
+                background-color: <?php echo get_theme_mod('footer_background_color', '#f5f5ed'); ?>;
+                color: <?php echo get_theme_mod('header_text_color', '#2f2f2f'); ?>;   
+            }
+         </style>
+    <?php
+}
+add_action( 'wp_head', 'cd_customizer_css');
---
+++ js/customizer.js
@@ -0,0 +1,38 @@
+(function($) {
+  /* --------- colors --------- */
+  wp.customize('main_background_color', function(value) {
+    value.bind(function(newval) {
+      $('body').css('background-color', newval);
+    });
+  });
+
+  wp.customize('header_background_color', function(value) {
+    value.bind(function(newval) {
+      $('.site-header').css('background-color', newval);
+    });
+  });
+
+  wp.customize('footer_background_color', function(value) {
+    value.bind(function(newval) {
+      $('footer').css('background-color', newval);
+    });
+  });
+
+  wp.customize('main_text_color', function(value) {
+    value.bind(function(newval) {
+      $('body').css('color', newval);
+    });
+  });
+
+  wp.customize('header_text_color', function(value) {
+    value.bind(function(newval) {
+      $('.site-header').css('color', newval);
+    });
+  });
+
+  wp.customize('footer_text_color', function(value) {
+    value.bind(function(newval) {
+      $('footer').css('color', newval);
+    });
+  });
+})(jQuery);
